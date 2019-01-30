d57695c30e4e32e2005c4257d56a10cda0f33946
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-01-30 17:00:49 -0500

    Update Classes

--- src/com/company/BookApp.java
+++ src/com/company/BookApp.java
@@ -1,13 +1,10 @@
 package com.company;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 /**
- * Assignment
- * ----------
  * Create a method that returns the pricing for a requested number of books. Five books at $20.00 should return
  * $100, if they are in stock. If they are not in stock, that should be handled appropriately (hint - you decide).
  *
@@ -15,16 +12,25 @@ import java.util.Scanner;
 
 public class BookApp {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter books ");
-        int numberOfBooks = keyboard.nextInt();
-
         BookDatabase database = new BookDatabase();
-        List<Book> books = database.getBooks();
+        List<Book> databaseBookList = database.getBooks();
 
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter  5 books ");
+
+        String bookNameInput, bookName;
+        List<Book> customerBookList = new ArrayList<>();
+
+        for(Book book:databaseBookList){
+            bookNameInput = keyboard.next();
+            bookName = book.getTitle();
+            if(bookName.contains(bookNameInput)){
+                customerBookList.add(book);
+            }
+        }
         BookApp app = new BookApp();
-        System.out.println("The total price of five books are "+app.getTotal(books));
-    }
+        System.out.println("The total price of five books are "+app.getTotal(customerBookList));
+   }
 
     public double getTotal(List<Book> books){
         int sum = 0;
