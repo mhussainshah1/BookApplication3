2d99037ead0489e96fd9a2b94a9e90c04a93e46b
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-01-30 21:32:31 -0500

    Initial Commit

--- src/com/company/BookDatabase.java
+++ src/com/company/BookDatabase.java
@@ -17,14 +17,76 @@ import java.util.List;
  *
  * Your database should contain the following books:
  *
- * SKU	Title	Author	Description	Price
- * --------------------------------------
- * Java1001	Head First Java	Kathy Sierra and Bert Bates	Easy to read Java workbook	47.50
- * Java1002	Thinking in Java	Bruce Eckel	Details about Java under the hood	20.00
- * Orcl1003	OCP: Oracle Certified Professional Java SE	Jeanne Boyarsky	Everything you need to know in one place	45.00
- * Python1004	Automate the Boring Stuff with Python	Al Sweigart	Fun with Python	10.50
- * Zombie1005	The Maker's Guide to the Zombie Apocalypse	Simon Monk	Defend Your Base with Simple Circuits, Arduino, and Raspberry Pi	16.50
- * Rasp1006	Raspberry Pi Projects for the Evil Genius	Donald Norris	A dozen fiendishly fun projects for the Raspberry Pi!	14.75
+ *
+ * <table>
+ *
+ * <thead>
+ * <tr>
+ *      <th>SKU</th>
+ *      <th>Title</th>
+ *      <th>Author</th>
+ *      <th>Description</th>
+ *      <th>Price</th>
+ * </tr>
+ * </thead>
+ *
+ * <tbody>
+ *
+ * <tr>
+ *      <td>Java1001</td>
+ *      <td>Head First Java</td>
+ *      <td>Kathy Sierra and Bert Bates</td>
+ *      <td>Easy to read Java workbook</td>
+ *      <td>47.50</td>
+ * </tr>
+ *
+ * <tr>
+ *      <td>Java1002</td>
+ *      <td>Thinking in Java</td>
+ *      <td>Bruce Eckel</td>
+ *      <td>Details about Java under the hood</td>
+ *      <td>20.00</td>
+ * </tr>
+ * <tr>
+ *     <td>Orcl1003</td>
+ *     <td>OCP: Oracle Certified Professional JavaSE.</td>
+ *     <td>Jeanne Boyarsky</td>
+ *     <td>Everything you need to know in one place</td>
+ *     <td>45.00</td>
+ * </tr>
+ *
+ *  <tr>
+ *      <td>Python1004</td>
+ *      <tr>Automate the Boring Stuff with Python</td>
+ *      <tr>Al Sweigart</td>
+ *      <tr>Fun with Python	</td>
+ *      <tr>10.50</td>
+ *  </tr>
+ *  <tr>
+ *      <td>Zombie1005</td>
+ *      <tr>The Maker's Guide to the Zombie Apocalypse</td>
+ *      <tr>Simon Monk</td>
+ *      <tr>Defend Your Base with Simple Circuits, Arduino, and Raspberry Pi</td>
+ *      <tr>16.50</td>
+ *  </tr>
+ *  <tr>
+ *      <td>Rasp1006</td>
+ *      <td>Raspberry Pi Projects for the Evil Genius</td>
+ *      <td>Donald Norris</td>
+ *      <td>A dozen fiendishly fun projects for the Raspberry Pi!</td>
+ *      <td>14.75</td>
+ *  </tr>
+ *
+ * </tbody>
+ *
+ * <tfoot>
+ * <tr>
+ * <td>Name</td>
+ * <td colspan="2">Address</td>
+ * </tr>
+ * </tfoot>
+ * </table>
+ *
  *
  */
 public class BookDatabase {
